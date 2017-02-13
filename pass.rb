29efb39a6e211498f568503fef00e798e4c15e35
Kevin Curtis <kevinjcurtis@lewisu.edu>
2017-02-13 20:43:53 +0000

    pass program

---
+++ pass.rb
@@ -0,0 +1,15 @@
+def pass? (grade)
+    if grade % 2 ==0
+        return true
+else
+    return false
+    end
+end 
+1.times do 
+grade = Random.rand(101)
+if pass?(grade) == false
+    puts"Failed the test"
+else
+    puts"Passed the test"
+end
+end
